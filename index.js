9c2c7b5dac440b9cc59803158cbf2e8e728bde84
GuillermoCzza <guiczza@gmail.com>
2023-02-01 23:32:20 -0300

    fix: remove leftover example code

--- index.js
+++ index.js
@@ -20,12 +20,6 @@ app.get("/", function(req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-
 
 //handle the timestamp request
 app.get("/api/:date?", function(req, res) {
